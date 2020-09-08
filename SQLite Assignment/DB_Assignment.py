4244dd996a2ff96cd837909d649b725b061b5b0d
CL-75 <49925072+CL-75@users.noreply.github.com>
2020-09-08 14:01:25 -0400

    Create DB_Assignment.py

---
+++ SQLite Assignment/DB_Assignment.py
@@ -0,0 +1,57 @@
+#
+#   Python Version: Python 3.8.5
+#
+#   Author: Casey Levy
+#
+#
+#   Purpose: SQLite and Python assignment creatng a database
+#           and modifying data
+#
+#
+#
+#
+
+import sqlite3
+
+
+conn = sqlite3.connect('DB_Assignment.db')
+
+fileList = ('information.docx', 'Hello.txt', 'myImage.png', \
+            'myMovie.mpg', 'World.txt', 'data.pdf', 'myPhoto.jpg')
+
+
+"""
+# connecting to DB and creating table with column
+"""
+with conn:
+    db = conn.cursor()
+    db.execute("CREATE TABLE IF NOT EXISTS tbl_files( \
+        ID INTEGER PRIMARY KEY AUTOINCREMENT, \
+        col_files TEXT \
+        )")
+
+"""
+# Specifying which file extension to add to DB, in this case it is .txt
+"""
+    for myFiles in fileList:
+        if myFiles.endswith(".txt"):
+            print(myFiles)
+            db.execute("INSERT INTO tbl_files(col_files) VALUES (?)", (myFiles,))
+            
+"""
+# Printing the selected files 
+"""
+    with conn:
+        db = conn.cursor()
+        db.execute("SELECT col_files FROM tbl_files")
+        printAll = db.fetchall()
+        print(printAll)
+    conn.commit()
+conn.close()
+
+
+
+
+
+
+
