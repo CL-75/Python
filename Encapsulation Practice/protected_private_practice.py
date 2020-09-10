de5847ce19b82276ee9e46a7842732b8c467551b
CL-75 <49925072+CL-75@users.noreply.github.com>
2020-09-10 09:49:25 -0400

    Update protected_private_practice.py

--- Encapsulation Practice/protected_private_practice.py
+++ Encapsulation Practice/protected_private_practice.py
@@ -1,33 +1,18 @@
+class Store:
+    def __init__(self, salary, bonusRate):
+        self._salary = salary     # protected
+        self.__bonusRate = bonusRate    # private
+        
+    def printBonusRate(self):
+        print(self.__bonusRate)
 
-# Protected class
-class Store(self):
-    def __init__(self, employee, salary):
-        self._employee = employee
-        self._salary = salary
-
-
-# Not entirely sure of the how correct the code below is
-
-# Private class
-class Store2(self):
-    def __init__(self, employee, salary):
-        self.__employee = employee
-        self.__salary = salary
-
-    def getBonus(self, bonusRate, bonusAmt):
-        self.__bonusRate = bonusRate
-        bonusRate = .3
-        self.__bonusAmt = bonusAmt
-        bonusAmt = salary * bonusRate
-        print(bonusAmt)
+    def salaryWithBonus(self):
+        total = (self._salary * self.__bonusRate) + self._salary
+        print(total)
 
 
-# Creating objects
-e1 = Store("John", 10000)
-e2 = Store("Mary", 12000)
 
-e1=Store2("Sarah", 11500)
-e2=Store2("Mark", 10050)
-e1.getBonus()
-e2.getBonus()
-
+A = Store(30000, .05)
+print(A._salary)
+A.printBonusRate()
+A.salaryWithBonus()
