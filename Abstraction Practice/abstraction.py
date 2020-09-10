73847c6f34649d4f9764d00fe475d2a411970466
CL-75 <49925072+CL-75@users.noreply.github.com>
2020-09-10 09:19:08 -0400

    Update abstraction.py

--- Abstraction Practice/abstraction.py
+++ Abstraction Practice/abstraction.py
@@ -1,27 +1,34 @@
 from abc import ABC, abstractmethod
 
 class Vehicle(ABC):
-  def action(self):
-    pass
-    
- class Car(Vehicle):
-   def action(self):
-    print("I can drive on the road")
+    def printNum(self,x):
+        print("Passed Value: ", x)
+    @abstractmethod
+    def action(self):
+        print("This is the Vehicle class")
+
+            
+class Car(Vehicle):
+    def action(self):
+        print("I can drive on the road")
     
 class Boat(Vehicle):
-  def action(self):
-    print("I can drive on the water")
+    def action(self):
+        print("I can travel on the water")
     
 class Plane(Vehicle):
-  def action(self):
-    print("I can fly in the air")
+    def action(self):
+        print("I can fly in the air")
     
     
 C = Car()
+C.printNum(100)
 C.action()
 
 B = Boat()
+B.printNum(400)
 B.action()
 
 A = Plane()
+A.printNum(120)
 A.action()
